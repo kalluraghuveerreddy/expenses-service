3034730efbf59762a7a6c05d72dc3bca5d87b816
kalluraghuveerreddy <kalluraghureddy@gmail.com>
2024-11-02 08:12:29 +0530

    second version

---
+++ src/main/java/com/aliencode/expensesservice/dto/HumanDto.java
@@ -0,0 +1,11 @@
+package com.aliencode.expensesservice.dto;
+
+public class HumanDto {
+
+    private String name;
+    private int age;
+    private String username;
+    private String email;
+    private String country;
+
+}
---
+++ src/main/java/com/aliencode/expensesservice/entity/Human.java
@@ -0,0 +1,11 @@
+package com.aliencode.expensesservice.entity;
+
+public class Human {
+
+    private Long id;
+    private String name;
+    private int age;
+    private String username;
+    private String email;
+    private String country;
+}
