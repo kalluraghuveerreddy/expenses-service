e570c347b9af7a6e23c4c44faf1139f1b0ed5c3d
Kallu Raghuveer Reddy <34940200+kalluraghuveerreddy@users.noreply.github.com>
2024-12-18 18:16:13 +0530

    feat(Ticket-123) give the fix. (#3)

--- src/main/java/com/aliencode/expensesservice/dto/HumanRequestDto.java
+++ src/main/java/com/aliencode/expensesservice/dto/HumanRequestDto.java
@@ -10,13 +10,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HumanRequestDto {
-
     private String name;
     private int age;
     private String username;
     private String email;
     private String gender;
     private String country;
+    private String pinCode;
     private String state;
-
 }
